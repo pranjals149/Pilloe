8ba8d158e82d7eb1bf6a9d12bfb001007e15c616
pranjals149 <pranjals149@gmail.com>
2021-10-31 02:22:54 +0530

    Google Auth

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,9 +1,35 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { setUserLoginDetails } from '../../features/user/userSlice';
+import { auth, provider } from '../../firebase';
 
 import "./HomePage.css"
 
 function HomePage() {
+
+    const history = useHistory()
+    const dispatch = useDispatch()
+
+    const googleAuth = () => {
+        auth.signInWithPopup(provider)
+            .then((res) => {
+                history.push('/options')
+                dispatch(
+                    setUserLoginDetails({
+                        id: res.user.uid,
+                        name: res.user.displayName,
+                        email: res.user.email,
+                        photo: res.user.photoURL
+                    })
+                )
+
+            })
+            .catch((err) => toast.error(err));
+    }
+
     return (
         <div className="home">
             <div className="home__heading">
@@ -11,7 +37,7 @@ function HomePage() {
             </div>
 
             <div className="home__buttonContainer">
-                <Button color="primary" variant="contained">Sign In to get started</Button>
+                <Button color="primary" variant="contained" onClick={googleAuth}>Sign In to get started</Button>
             </div>
         </div>
     )
